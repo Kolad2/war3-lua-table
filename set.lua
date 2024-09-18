01a845f1348761ca3fb08c6dc57cf6afdc8cb066
Kolad2 <koladik@gmail.com>
2024-09-19 02:33:02 +0300

    set update

--- set.lua
+++ set.lua
@@ -12,33 +12,30 @@ do
                 return false
             end
             table.insert(self, item)
-            self:set(item, #self)
+            self.__index_table:set(item, #self)
             return true
         end
-        self.add = self.append
+        self.add = self.insert
 
         ---@param idx number
         ---@return boolean
-        function self:pop_swap(idx)
-            local item = self:super_pop_swap(idx)
-            if item~=nil then
-                if self[idx]~=nil then self:_set_index(self[idx], idx) end
-                self:_clear_index(item)
-                return true
-            end
-            return false
-        end
-
-        ---@param item any
         function self:remove(item)
-            local idx = self:_get_index(item)
+            local idx = self.__index_table:get(item)
             if not idx then
                 print("Ошибка: элемент не существовал")
                 return false
             end
-            return self:pop_swap(idx)
+            table.remove_swap(self, idx)
+            if self[idx] ~= nil then
+                self.__index_table:remove(item)
+                self.__index_table:set(self[idx], idx)
+                return true
+            else
+                self.__index_table:remove(item)
+            end
+            return false
         end
 
-        return setmetatable(self, meta)
+        return self -- setmetatable(self, meta)
     end
 end
