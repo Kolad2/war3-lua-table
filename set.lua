075d444a468f9003955a24e6673f1384ce6bd362
Kolad2 <koladik@gmail.com>
2024-09-24 11:04:25 +0300

    set update

--- set.lua
+++ set.lua
@@ -1,10 +1,18 @@
 do
     ---@class set
     ---@field remove fun(self:set, item:any): boolean
-    local object = {}
+    ---@field insert fun(self:set, item:any): boolean
+    ---@field add fun(self:set, item:any): boolean
+    ---@field has fun(self:set, item:any): boolean
+    ---@field get_random fun(self:set): any
+    ---@field remove_random fun(self:set)
+    ---@field create fun(self:set, tbl:table): set
+    ---@overload
+    set = set or {}
 
     ---@param item any
-    function object:insert(item)
+    ---@return boolean
+    function set:insert(item)
         if self.__index_table:has(item) then
             return false
         end
@@ -14,7 +22,8 @@ do
     end
 
     ---@param item any
-    function object:add(item)
+    ---@return boolean
+    function set:add(item)
         if self:insert(item) then
             return true
         end
@@ -24,13 +33,14 @@ do
 
     ---@param item any
     ---@return boolean
-    function object:has(item)
+    function set:has(item)
         return self.__index_table:has(item)
     end
 
-    ---@param item any
-    ---@return boolean
-    function object:remove(item)
+    --- Removes an item from the set.
+    ---@param item any The item to remove.
+    ---@return boolean Returns `true` if the item was successfully removed, `false` otherwise.
+    function set:remove(item)
         local idx = self.__index_table:get(item)
         if not idx then
             print("Ошибка: элемент не существовал")
@@ -48,28 +58,29 @@ do
     end
 
 
-    function object:get_random()
+    function set:get_random()
         return table.get_random(self)
     end
 
-    function object:remove_random()
+    function set:remove_random()
         return self:remove(self:get_random())
     end
 
 
-    local object_meta = {
-        __index = object,
+    set.object_meta = {
+        __index = set,
         __tostring = table.tostring
     }
 
 
-    ---set
-    ---@return set
-    function set(tbl)
-        --local tbl, index_table = table.unique(tbl)
-        local self = {} ---@type set
-        self.__index_table = dict()
-
-        return setmetatable(self, object_meta)
+    --- Создаёт новое множество.
+    ---@param tbl? table Начальная таблица элементов (опционально).
+    ---@return set Новое множество.
+    function set:create(tbl)
+        tbl = tbl or {} ---@type set
+        tbl, tbl.__index_table = table.unique(tbl)
+        return setmetatable(obj, self.object_meta)
     end
+
+    setmetatable(set, {__call = set.create})
 end
