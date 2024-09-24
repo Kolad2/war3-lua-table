439f5398f6f4c0187d9756a17be6a020a3cdc5fa
Kolad2 <koladik@gmail.com>
2024-09-24 19:14:58 +0300

    ndarray readme update luadoc update

--- dict.lua
+++ dict.lua
@@ -1,36 +1,107 @@
 do
+    ---@class dict
+    ---@field create fun(self:dict)
+    ---@field set fun(self:dict, key:number|string, item:any):void
+    ---@field remove fun(self:dict, key:number|string):void
+    ---@field has fun(self:dict, key:number|string):boolean
+    ---@field get fun(self:dict, key:number|string):any
+    ---@field each fun(self:dict, callback:fun(key:number|string, value:any)): void
+    ---@field keys fun(self:dict): table
+    ---@field values fun(self:dict): table
+    ---@field size fun(self:dict): number
+    ---@field clear fun(self:dict): void
+    dict = dict or {}
 
 
-    local object = {} ---@type dict
-
-    ---@param key any
+    ---set
+    ---@param key number|string
     ---@param item any
-    function object:set(key, item)
+    function dict:set(key, item)
         self.data[key] = item
     end
 
-    function object:remove(key)
+    ---remove
+    ---@param key number|string
+    function dict:remove(key)
         self.data[key] = nil
     end
 
-    function object:has(key)
+    ---has
+    ---@param key number|string
+    function dict:has(key)
         return self.data[key] ~= nil
     end
 
-    function object:get(key)
+    ---get
+    ---@param key number|string
+    ---@return any
+    function dict:get(key)
         return self.data[key]
     end
 
-    local dict_meta = {
+    -- Итератор
+    ---@param callback fun(key:number|string, value:any)
+    function dict:each(callback)
+        for key, value in pairs(self.data) do
+            callback(key, value)
+        end
+    end
+
+    -- Получение ключей
+    ---@return table
+    function dict:keys()
+        local keys = {}
+        for key in pairs(self.data) do
+            table.insert(keys, key)
+        end
+        return keys
+    end
+
+
+    -- Получение значений
+    ---@return table
+    function dict:values()
+        local values = {}
+        for _, value in pairs(self.data) do
+            table.insert(values, value)
+        end
+        return values
+    end
+
+
+    -- Размер словаря
+    ---@return number
+    function dict:size()
+        local count = 0
+        for _ in pairs(self.data) do
+            count = count + 1
+        end
+        return count
+    end
+
+    -- Очистка словаря
+    function dict:clear()
+        for key in pairs(self.data) do
+            self.data[key] = nil
+        end
+    end
+
+    dict.__meta = {
         __index = object
     }
 
+    ---create
+    ---@param tbl table|nil
     ---@return dict
-    function dict()
-        self = {
-            data = {}
-        }
-        return setmetatable(self, dict_meta)
+    function dict:create(tbl)
+        local obj = {
+            data = tbl or {}
+        } ---@type dict
+        return setmetatable(obj, self.__meta)
     end
 
+    local dict_meta = {
+        __call = dict.create
+    }
+    setmetatable(dict, dict_meta)
 end
