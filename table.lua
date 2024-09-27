d49103c6ac2ef4616813432d59e49faac6994e2a
Kolad2 <koladik@gmail.com>
2024-09-27 03:07:47 +0300

    update 1

--- table.lua
+++ table.lua
@@ -1,7 +1,9 @@
 do
     --[[war3-lua-table (20.09.2024)]]--
     ---@class table
+    ---@field unique fun(tbl: table): table, table
     ---@field find fun(tbl: table, value: any): table
+    ---@field find_first fun(tbl: table, value: any, pos_start: number): table|nil
     ---@field get_meta_compatible fun(tbl:table,...):table
     ---@field merge fun(tbl:table,...):table
     ---@field get_random fun(tbl:table):any
@@ -33,7 +35,7 @@ do
     table = table or {}
 
     ---@return table
-    function table.empty()
+    table.empty = function()
         return {}
     end
     
@@ -42,7 +44,8 @@ do
     ---@param pos number
     ---@param value number
     ---@return table
-    table.insert = table.insert or function(tbl, pos, value)
+    table.insert = table.insert or
+    function(tbl, pos, value)
         -- Определяем, передано ли значение или только позиция
         if value == nil then
             pos, value = #tbl + 1, pos
@@ -66,24 +69,27 @@ do
     ---@param start_pos number
     ---@param end_pos number
     ---@return table
-    function table.unpack(tbl, start_pos, end_pos)
+    table.unpack = table.unpack or
+    function(tbl, start_pos, end_pos)
         start_pos = start_pos or 1
         end_pos = end_pos or #tbl
         if start_pos <= end_pos then
-            return tbl[i], table.unpack(tbl, start_pos + 1, end_pos)
+            return tbl[start_pos], table.unpack(tbl, start_pos + 1, end_pos)
         end
     end
 
     ---pack
     ---@return table
-    function table.pack(...)
+    table.pack = table.pack or
+    function(...)
         return { ... }
     end
 
     ---tostring
     ---@param tbl table
     ---@return table
-    function table.tostring(tbl)
+    table.tostring = table.tostring or
+    function(tbl)
         if tbl == nil then return tostring(nil) end
         if type(tbl) ~= "table" then return tostring(tbl) end
         local str = "{"
@@ -116,7 +122,8 @@ do
     ---@param tbl table
     ---@param idx number
     ---@return any
-    function table.remove_swap(tbl, idx)
+    table.remove_swap = table.remove_swap or
+    function(tbl, idx)
         idx = idx or #tbl
         local item = tbl[idx]
         tbl[idx]  = tbl[#tbl]
@@ -128,7 +135,8 @@ do
     ---shuffle
     ---@param tbl table
     ---@return table
-    function table.shuffle(tbl)
+    table.shuffle = table.shuffle or
+    function(tbl)
         -- алгоритм случайного тасования Дурштенфельда
         for i = #tbl, 2, -1 do
             local j = math.random(1, i)
@@ -139,7 +147,8 @@ do
     ---reverse
     ---@param tbl table
     ---@return table
-    function table.reverse(tbl)
+    table.reverse = table.reverse or
+    function(tbl)
         if #tbl <= 1 then return end
         local n = math.floor(#tbl/2)
         for i=1, n do
@@ -150,7 +159,8 @@ do
     ---sum
     ---@param tbl table
     ---@return number
-    function table.sum(tbl)
+    table.sum = table.sum or
+    function(tbl)
         local sum = 0
         for i=1, #tbl do
             sum = sum + tbl[i]
@@ -161,7 +171,8 @@ do
     ---cumsum
     ---@param tbl table
     ---@return table
-    function table.cumsum(tbl)
+    table.cumsum =
+    function(tbl)
         local cs = {}
         local sum = 0
         for i=1, #tbl do
@@ -174,7 +185,8 @@ do
     ---max
     ---@param tbl table
     ---@return table
-    function table.max(tbl)
+    table.max =
+    function(tbl)
         local len = #tbl
         if len == 0 then return nil end
         if len == 1 then return tbl[1] end
@@ -188,7 +200,8 @@ do
     ---min
     ---@param tbl table
     ---@return table
-    function table.min(tbl)
+    table.min =
+    function(tbl)
         local len = #tbl
         if len == 0 then return nil end
         if len == 1 then return tbl[1] end
@@ -202,7 +215,8 @@ do
     ---argmax
     ---@param tbl table
     ---@return table
-    function table.argmax(tbl)
+    table.argmax =
+    function(tbl)
         local len = #tbl
         if len == 0 then return nil end
         if len == 1 then return 1 end
@@ -216,7 +230,8 @@ do
     ---argmin
     ---@param tbl table
     ---@return table
-    function table.argmin(tbl)
+    table.argmin =
+    function(tbl)
         local len = #tbl
         if len == 0 then return nil end
         if len == 1 then return 1 end
@@ -232,7 +247,8 @@ do
     ---@param tbl table
     ---@param value number
     ---@return table
-    function table.add(tbl, value)
+    table.add =
+    function(tbl, value)
           for i=1, #tbl do
             tbl[i] = tbl[i] + value
           end
@@ -244,7 +260,8 @@ do
     ---@param tbl table
     ---@param val number
     ---@return table
-    function table.subtract(tbl, val)
+    table.subtract =
+    function(tbl, val)
         for i=1, #tbl do
             tbl[i] = tbl[i] - val
         end
@@ -256,7 +273,8 @@ do
     ---@param tbl table
     ---@param val number
     ---@return table
-    function table.multiply(tbl, val)
+    table.multiply =
+    function(tbl, val)
         for i=1, #tbl do
             tbl[i] = tbl[i] * val
         end
@@ -268,7 +286,8 @@ do
     ---@param tbl table
     ---@param val number
     ---@return table
-    function table.divide(tbl, val)
+    table.divide =
+    function(tbl, val)
         for i=1, #tbl do
             tbl[i] = tbl[i] / val
         end
@@ -279,7 +298,8 @@ do
     ---@param tbl table
     ---@param pos_start number
     ---@param pos_end number
-    function table.slice(tbl, pos_start, pos_end)
+    table.slice =
+    function(tbl, pos_start, pos_end)
         local slice = {}
         local length = pos_end - pos_start + 1
         local _pos_start = pos_start - 1
@@ -292,7 +312,8 @@ do
     ---copy
     ---@param tbl table
     ---@return table
-    function table.copy(tbl)
+    table.copy = table.copy or
+    function(tbl)
         local tbl_copy = {}
         for key, value in pairs(tbl) do
             if type(value) == "table" then
@@ -304,10 +325,31 @@ do
         return setmetatable(tbl_copy, getmetatable(tbl))
     end
 
+
+    ---deepcopy
+    ---@param tbl table
+    ---@return table
+    table.deepcopy = table.deepcopy or
+    function(tbl)
+        local tbl_copy = {}
+        for key, value in pairs(tbl) do
+            if type(value) == "table" then
+                local meta = getmetatable(tbl)
+                local deepcopy = (meta ~= nil) and meta.__deepcopy or table.deepcopy
+                tbl_copy[key] = deepcopy(value)
+            else
+                tbl_copy[key] = value
+            end
+        end
+        return setmetatable(tbl_copy, getmetatable(tbl))
+    end
+
+
     ---is_sorted
     ---@param tbl table
     ---@return boolean
-    function table.is_sorted(tbl)
+    table.is_sorted =
+    function(tbl)
         for i = 2, #tbl do
             if tbl[i - 1] >= tbl[i] then
                 return false
@@ -323,7 +365,8 @@ do
     ---@param pos_to number
     ---@param tbl_to table
     ---@return table
-    function table.move(tbl, pos_start, pos_end,  pos_to, tbl_to)
+    table.move =
+    function(tbl, pos_start, pos_end,  pos_to, tbl_to)
         tbl_to = tbl_to or tbl
         pos_to = pos_to or 1
         
@@ -342,10 +385,12 @@ do
 
     ---fill
     ---@param tbl table
-    ---@param value number
-    ---@param pos_start number
-    ---@param pos_end number
-    function table.fill(tbl, value, pos_start, pos_end)
+    ---@param value number|function
+    ---@param pos_start number|nil
+    ---@param pos_end number|nil
+    table.fill =
+    function(tbl, value, pos_start, pos_end)
+        pos_start = pos_start or 1
         if pos_end == nil then pos_start, pos_end = 1, pos_start or #tbl end
         if type(value) == "number" then
             for i = pos_start, pos_end do
@@ -353,7 +398,7 @@ do
             end
         else
             for i = pos_start, pos_end do
-                tbl[i] = value()
+                tbl[i] = value(i)
             end
         end
     end
@@ -362,21 +407,34 @@ do
     ---@param tbl table
     ---@param idx number
     ---@return any
-    function table.get(tbl, idx)
+    table.get =
+    function(tbl, idx)
         return tbl[idx]
+    end
+
+
+    ---rawget
+    ---@param tbl table
+    ---@param idx number
+    ---@return any
+    table.rawget =
+    function(tbl, idx)
+        return rawget(tbl, idx)
     end
 
 
     ---get_random
     ---@param tbl table
     ---@return any
-    function table.get_random(tbl)
+    table.get_random =
+    function(tbl)
         if #tbl == 0 then return nil end
         return tbl[math.random(1, #tbl)]
     end
 
-    
-    function table.get_meta_compatible(tbl,...)
+
+    table.get_meta_compatible =
+    function(tbl,...)
         local tbls = {tbl, ...}
         if #tbls == 1 then tbls = {table.unpack(tbl)} end
         
@@ -392,8 +450,8 @@ do
         return meta or getmetatable(tbl)
     end
 
-    
-    function table.merge(tbl, ...)
+
+    table.merge = function(tbl, ...)
         if tbl == nil then return nil end
         -- try to set common metatable
         local meta = table.get_meta_compatible(tbl,...)
@@ -418,24 +476,49 @@ do
         return merge
     end
 
-
-    function table.unique(tbl)
-        local index_table = dict()
+    ---unique
+    ---@param tbl table
+    ---@return table, dict
+    table.unique =
+    function(tbl)
+        local index_dict = dict()
         local i = 1
         while i <= #tbl do
             local item = tbl[i]
-            if not (index_table:has(item)) then
-                index_table:set(item, i)
+            if not (index_dict:has(item)) then
+                index_dict:set(item, i)
                 i = i + 1
             else
                 table.remove_swap(tbl, i)
             end
         end
-        return tbl, index_table
+        return tbl, index_dict
     end
 
 
-    table.find = table.find or function(tbl, item)
+    ---find_first
+    ---@overload fun(tbl:table, item:any):number|nil
+    ---@param tbl table
+    ---@param item any
+    ---@param pos_start|nil
+    ---@return number|nil
+    table.find_first = table.find_first or
+    function(tbl, item, pos_start)
+        pos_start = pos_start or 1
+        for i =  pos_start, #tbl do
+            if tbl[i] == item then
+                return i
+            end
+        end
+        return nil
+    end
+
+    ---find
+    ---@param tbl table
+    ---@param item any
+    ---@return number[]
+    table.find = table.find or
+    function(tbl, item)
         local indices  = {}
         for i = 1, #tbl do
             if tbl[i] == item then
